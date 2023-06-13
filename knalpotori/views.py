60aab4802df52c12c0632e3d784f9949143ffc75
Afadfath03 <Afadfath03@Gmail.com>
2023-06-13 23:02:28 +0700

    Simplified Code

--- knalpotori/views.py
+++ knalpotori/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
+""" def index(request):
     template = loader.get_template('beranda.html')
     context = {
         'title' : 'Django | Pemrograman 1',
@@ -12,9 +12,26 @@ def index(request):
         'product_name' : 'Knalpot Original',
         'product_list' : [1,2,3,4,5],
     }
-    return HttpResponse(template.render(context,request))
-
+    return HttpResponse(template.render(context,request)) 
+    
 def login(request):
     template = loader.get_template('login.html')
     context = {}
     return HttpResponse(template.render(context,request))
+    
+"""
+
+# Code diatas dapat disederhanakan menjadi seperti dibawah ini 
+
+def index (request):
+    context = {
+    'title' : 'Django | Pemrograman 1',
+    'img_product' : 'https://cdn.juraganwp.com/aripitstop/wp-content/uploads/2018/08/knalpot-indolingga-2.jpg',
+    'product_name' : 'Knalpot Original',
+    'product_list' : [1,2,3,4,5],
+    }
+    return render(request, 'beranda.html', context)
+    
+def login(request):
+    context = {}
+    return render(request, 'login.html', context)
