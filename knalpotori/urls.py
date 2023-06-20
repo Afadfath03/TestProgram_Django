9517e3124d210209cbf46470d5fe33439a0409bb
Afadfath03 <Afadfath03@Gmail.com>
2023-06-20 10:05:29 +0700

    Update path

--- knalpotori/urls.py
+++ knalpotori/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.login, name="login"),   
+    path("/", views.login, name="login"),   
     path("beranda/", views.index, name="beranda")   
 ]
