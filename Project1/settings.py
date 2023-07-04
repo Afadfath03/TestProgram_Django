19e2f7f80ea7ce3132b07b038f7f7383229b9180
Afadfath03 <Afadfath03@Gmail.com>
2023-07-04 13:41:59 +0700

    Connecting to Database phpmyadmin

--- Project1/settings.py
+++ Project1/settings.py
@@ -76,10 +76,24 @@ WSGI_APPLICATION = 'Project1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_knalpotori',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+     }
     }
 }
 
